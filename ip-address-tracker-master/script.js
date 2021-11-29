bc49c292a0b7756619781263bdb390fa6f454e5d
GodzFeivur <godzfeivvur@gmail.com>
2021-11-29 15:25:41 +0100

    funtionality completed

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,49 +1,68 @@
-var mymap = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+const submitBtn = document.querySelector('.btn-submit');
+submitBtn.addEventListener("click", getIPAddress);
 
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
 
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
+// Setting up the map
+var map = L.map('mapid').setView([51.505, -0.09], 16);
+L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=JXZcJEvrrHAGuMvPU8me', {
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(map);
 
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+function getIPAddress(e) {
+    // constructing the url
+    let userInput = document.querySelector('.user-address').value;
+    const url = 'https://geo.ipify.org/api/v2/country?apiKey=';
+    const apiKey = 'at_k6EO4BJ2k2gheBCBmOFBC6z1kLrZB';
 
-var popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(mymap);
+    // if input does not end in a number it is a domain
+    if (!isNaN(userInput.slice(-1))) {
+        // if contains valid number
+        var domainOrIP = '&ipAddress=';
+    } else {
+        var domainOrIP = '&domain=';
+    }
 
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
+    let fullURL = 'https://geo.ipify.org/api/v2/country,city?apiKey=' + apiKey + domainOrIP + userInput;
+    let ipAddressText = document.querySelector('#ip-address-text');
+    let locationText = document.querySelector('#location-text');
+    let timezoneText = document.querySelector('#timezone-text');
+    let ispText = document.querySelector('#isp-text');
+
+    fetch(fullURL)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error("Invalid input.");
+            }
+        })
+        .then(data => {
+            ipAddressText.innerHTML = data.ip;
+            ispText.innerHTML = data.isp;
+            timezoneText.innerHTML = 'UTC ' + data.location.timezone;
+            locationText.innerHTML = data.location.city + ', ' + data.location.region + ' ' + data.location.postalCode;
+
+            // changing map location and setting marker
+            map.panTo(new L.LatLng(data.location.lat, data.location.lng));
+            var marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+        })
+        .catch((error) => {
+            console.log(error);
+            inputError(error);
+        });
 }
 
-mymap.on('click', onMapClick);
-
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
+function inputError(error) {
+    let inputBox = document.querySelector('.user-address');
+    let errorMessage = document.querySelector('.error-msg');
+    inputBox.classList.add('error-input');
+    errorMessage.innerHTML = error;
+    setTimeout(removeErrorInput, 3000);
 }
 
-mymap.on('click', onMapClick);
+function removeErrorInput() {
+    let inputBox = document.querySelector('.user-address');
+    let errorMessage = document.querySelector('.error-msg');
+    inputBox.classList.remove('error-input');
+    errorMessage.innerHTML = '';
+}
